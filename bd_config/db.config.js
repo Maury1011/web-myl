e0be22be90b154c7369ef438b46a956a4fa08d0b
Mauricio Garrido <mauri101100@gmail.com>
2025-03-30 13:56:21 -0300

    Adaptando conexión a PostgreSQL en Railway

--- bd_config/db.config.js
+++ bd_config/db.config.js
@@ -1,11 +1,13 @@
 
 export const dbConfig = {
-    HOST: 'localhost', 
-    USER: 'postgres', 
-    PASSWORD: 'master009', 
-    DB: 'myl_db', 
-    DIALECT: 'postgres', 
+    DB: process.env.DB || '',  // No se usa directamente en Railway
+    USER: process.env.USER || '',  
+    PASSWORD: process.env.PASSWORD || '',
+    HOST: process.env.HOST || '',
+    DIALECT: 'postgres',
+    DATABASE_URL: process.env.DATABASE_URL  // Usa la URL de Railway
 };
+
 
 
 
