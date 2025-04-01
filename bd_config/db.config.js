8ce139bdf99bf3fc5a00d6f4a2fb45d59549941f
Mauricio Garrido <mauri101100@gmail.com>
2025-04-01 17:23:05 -0300

    Restaurar archivos de configuración de base de datos

---
+++ bd_config/db.config.js
@@ -0,0 +1,25 @@
+
+export const dbConfig = {
+    HOST: 'localhost', 
+    USER: 'postgres', 
+    PASSWORD: 'master009', 
+    DB: 'myl_db', 
+    DIALECT: 'postgres', 
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
