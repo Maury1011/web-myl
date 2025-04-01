035bb9d12555e40b7f9f553869154ec0401aec23
Mauricio Garrido <mauri101100@gmail.com>
2025-04-01 16:56:15 -0300

    aaa

--- bd_config/db.config.js
+++ bd_config/db.config.js
@@ -1,11 +1,10 @@
 
 export const dbConfig = {
-    DB: process.env.DB || '',  // No se usa directamente en Railway
-    USER: process.env.USER || '',  
-    PASSWORD: process.env.PASSWORD || '',
-    HOST: process.env.HOST || '',
-    DIALECT: 'postgres',
-    DATABASE_URL: process.env.DATABASE_URL  // Usa la URL de Railway
+    HOST: 'localhost', 
+    USER: 'postgres', 
+    PASSWORD: 'master009', 
+    DB: 'myl_db', 
+    DIALECT: 'postgres', 
 };
 
 
